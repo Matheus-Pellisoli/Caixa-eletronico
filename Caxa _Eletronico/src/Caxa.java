1ffa07c784bdb37f01fa4b8901032d3c1f1abea9
Matheus Pellisoli <matheuspellisoli@gmail.com>
2016-06-23 21:35:53 -0300

    Update Caxa.java

--- Caxa _Eletronico/src/Caxa.java
+++ Caxa _Eletronico/src/Caxa.java
@@ -51,8 +51,7 @@ public class Caxa {
 		System.out.println("Notas de 20:"+ nota_20);
 		System.out.println("Notas de 10:"+ nota_10);
 		System.out.println("Notas de 2:"+ nota_2);
-		System.out.println("Notas de 2:"+ 56);
-		System.out.println("Notas de 2:"+ 56);
+	
 	}
 	}
 
